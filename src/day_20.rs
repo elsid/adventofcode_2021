5cdee228a57278f4949ed2a047b013bdda0293c0
elsid <elsid.mail@gmail.com>
2021-12-20 14:11:39 +0100

    Add solutions for day 20

---
+++ src/day_20.rs
@@ -0,0 +1,121 @@
+use std::collections::BTreeMap;
+use std::io::BufRead;
+
+fn main() {
+    println!("{:?}", count_enhanced_light_pixels(std::io::stdin().lock()));
+}
+
+const FIRST_LIMIT: usize = 2;
+const SECOND_LIMIT: usize = 50;
+
+fn count_enhanced_light_pixels(buffer: impl BufRead) -> (usize, usize) {
+    let (enhancement, mut image) = parse_image(buffer);
+    let mut default_pixel = '0';
+    for _ in 0..FIRST_LIMIT {
+        image = enhance_image(&enhancement, &image, &mut default_pixel);
+    }
+    let first = image.values().filter(|v| **v == b'#').count();
+    for _ in FIRST_LIMIT..SECOND_LIMIT {
+        image = enhance_image(&enhancement, &image, &mut default_pixel);
+    }
+    (first, image.values().filter(|v| **v == b'#').count())
+}
+
+fn enhance_image(
+    enhancement: &[u8],
+    image: &BTreeMap<(isize, isize), u8>,
+    default_pixel: &mut char,
+) -> BTreeMap<(isize, isize), u8> {
+    let mut result = BTreeMap::new();
+    for (x, y) in image.keys() {
+        result.insert(
+            (*x, *y),
+            enhance_pixel(*x, *y, *default_pixel, enhancement, image),
+        );
+    }
+    let (min_x, min_y) = image.keys().next().unwrap().clone();
+    let (max_x, max_y) = image.keys().rev().next().unwrap().clone();
+    let mut shift = 1;
+    loop {
+        let mut light_count = 0;
+        let mut count = 0;
+        let mut add_pixel = |x, y| {
+            let value = enhance_pixel(x, y, *default_pixel, enhancement, image);
+            result.insert((x, y), value);
+            light_count += (value == b'#') as usize;
+            count += 1;
+        };
+        for x in min_x - shift..=max_x + shift {
+            for y in [min_y - shift, max_y + shift] {
+                add_pixel(x, y);
+            }
+        }
+        for y in min_y - shift..=max_y + shift {
+            for x in [min_x - shift, max_x + shift] {
+                add_pixel(x, y);
+            }
+        }
+        if light_count == 0 {
+            *default_pixel = '0';
+            break;
+        }
+        if light_count == count {
+            *default_pixel = '1';
+            break;
+        }
+        shift += 1;
+    }
+    result
+}
+
+fn enhance_pixel(
+    x: isize,
+    y: isize,
+    default_value: char,
+    enhancement: &[u8],
+    image: &BTreeMap<(isize, isize), u8>,
+) -> u8 {
+    let mut pixels = String::new();
+    for py in y - 1..=y + 1 {
+        for px in x - 1..=x + 1 {
+            match image.get(&(px, py)) {
+                Some(b'#') => pixels.push('1'),
+                Some(b'.') => pixels.push('0'),
+                _ => pixels.push(default_value),
+            }
+        }
+    }
+    enhancement[usize::from_str_radix(&pixels, 2).unwrap()]
+}
+
+fn parse_image(buffer: impl BufRead) -> (Vec<u8>, BTreeMap<(isize, isize), u8>) {
+    let mut enhancement = Vec::new();
+    let mut image = BTreeMap::new();
+    let mut y = 0isize;
+    for line in buffer.lines() {
+        let line_str = line.unwrap();
+        if enhancement.is_empty() {
+            enhancement = line_str.as_bytes().iter().copied().collect();
+        } else if !line_str.is_empty() {
+            for (x, byte) in line_str.as_bytes().iter().enumerate() {
+                image.insert((x as isize, y), *byte);
+            }
+            y += 1;
+        }
+    }
+    (enhancement, image)
+}
+
+#[test]
+fn example_test() {
+    let buffer = r#"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+"#
+    .as_bytes();
+    assert_eq!(count_enhanced_light_pixels(buffer), (35, 3351));
+}
